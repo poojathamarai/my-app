066b8bf9cd177caa68e691520a5f66f40d1c9c87
Pooja <92292344+poojathamarai@users.noreply.github.com>
2021-12-20 01:18:31 +0530

    Add files via upload

---
+++ src/MovieList.js
@@ -0,0 +1,23 @@
+import { Movie } from './Movie';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
+export function MovieList({ movies,setMovieList }) {
+  return (
+    <div className="movie-list">
+      {movies.map(({ name, poster, rating, summary },index) => (
+        <Movie deleteButton={
+            <IconButton
+             onClick={()=>{
+             const deleteIndex=index;
+             const remainingMovies=movies.filter((mv,idx)=>deleteIndex!=idx); 
+                setMovieList(remainingMovies) 
+     }}><DeleteIcon color="error"/></IconButton>
+     }
+          name={name}
+          poster={poster}
+          rating={rating}
+          summary={summary} />
+      ))}
+    </div>
+  );
+}
